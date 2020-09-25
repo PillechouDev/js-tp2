c326216b93b5baa81a0f0f89829f1550a5479005
YOHAN <widogue.yohan@gmail.com>
2020-09-25 12:21:55 +0200

    final

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const radioeur = document.querySelector('#eur');
 const radiofrf = document.querySelector('#frf');
-const valuer = document.querySelector('#amount').value;
 const p = document.createElement('p');
+const result = document.querySelector('#result');
 
 function convert(){
-    if(radioeur.checked){
-        p.innerText= `Resultat de FRF à € : ` 
-        console.log('test')
-    }
-    else if (radiofrf.checked){
-        console.log(radiofrf.value)
+    
+    if(document.querySelector('#amount').value == ""){
+        alert('Veuillez saisir un montant')
     }
     else{
-        alert('Veuillez selectionnez une deivse')
+        valuer=document.querySelector('#amount').value
+        if(radioeur.checked){
+            console.log(valuer)
+            a = valuer*0.15
+            console.log(a)
+            result.innerText = `Resultat de FRF à € : ` +a
+            console.log('test')
+        }
+        else if (radiofrf.checked){
+            a= valuer*6.56
+            result.innerText = `Resultat de € à FRF : ` +a
+        }
+        else{
+            alert('Veuillez selectionnez une deivse')
+        }
     }
+    
     
 }
